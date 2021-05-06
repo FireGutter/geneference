473dda9ca4a92e3d8547f03e82b23b69cac15ea8
jbirkesteen <jbsteenstrup@post.au.dk>
2021-05-06 17:27:25 +0200

    Linted and documented create_map

--- R/create_map.R
+++ R/create_map.R
@@ -1,22 +1,23 @@
 #'
 #' @title create_map
-#' @description This function creates a map file that is necessary for PLINK.
+#' @description As PLINK requires a .map-file in order to run its analysis,
+#' we create such a file and fill it with some arbitrary information, since
+#' it is not actually used for the analysis.
 #'
-#' Note change the first entry "data_map[, 1]" to have cromosones in the analysis.
-#'
-#' @param n is the amount of sequenced enteties.
-#' @param path The path where the file is going to be stored.
+#' @param n number of sequenced genotypes.
+#' @param path location where the file is going to be stored.
 #'
 #' @importFrom data.table as.data.table fwrite
 #'
-#' @return This function creates a map file
+#' @return Does not return anything, but creates a file with the name
+#' "genotypes.map" in the folder specified with 'path'-parameter.
 
-create_map <- function(n, path){
-  data_map <- matrix(ncol=4, nrow=n)
-  data_map[, 1] <- rep(1,n) # Cromosone code
-  data_map[, 2] <- seq(1:n) # Varient identifier
-  data_map[, 3] <- rep(0,n) # Dummy = 0
-  data_map[, 4] <- seq(1:n) # Base-pair coordiante
+create_map <- function(n, path) {
+  data_map <- matrix(ncol = 4, nrow = n)
+  data_map[, 1] <- rep(1, n) # Chromosome code
+  data_map[, 2] <- seq(1:n) # Variant identifier
+  data_map[, 3] <- rep(0, n) # Dummy with value 0
+  data_map[, 4] <- seq(1:n) # Base-pair coordinate
 
   fwrite(as.data.table(data_map), ## writes to locked file
          file = paste0(path, "genotypes.map", sep = ""),
