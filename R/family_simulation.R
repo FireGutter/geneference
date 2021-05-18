a2633b715d309e62707d2b3904a2c2ee8c0073fc
Rasmus <razz_bj@hotmail.com>
2021-05-18 20:38:51 +0200

    Update family_simulation.R
    implemented validation for sib

--- R/family_simulation.R
+++ R/family_simulation.R
@@ -64,6 +64,8 @@ family_simulation <- function(n, m, q, hsq, k, path = ".", sib = 0) {
               (hsq >= 0 && hsq <= 1 && class(hsq) == "numeric"))
   stopifnot("k needs to be a number between 0 and 1" = 
               (k > 0 && k < 1 && class(k) == "numeric"))
+  stopifnot("Sib needs to be a positive integer" =
+              (sib >= 0 && class(sib) == "numeric" && round(sib) == sib))
 
   # Defining a function that creates genotypes for parents
   parent_maker <- function(m, number, MAFs) {
