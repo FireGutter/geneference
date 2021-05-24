7153257d6bcc7d85fd7d36435893cb5ef4b980e7
jbirkesteen <jbsteenstrup@post.au.dk>
2021-05-24 21:47:50 +0200

    Wrote, linted and built package with error type function
    Documentation is just for a start. Parameters should be done.

---
+++ R/error_types.R
@@ -0,0 +1,31 @@
+#' @title Calculate the number of error types in an analysis
+#'
+#' @description description to be written.
+#'
+#' @param dataset data loaded with \code{load_assoc_results()}.
+#' @param alpha significance level used for hypothesis tests.
+#' @param analysis_name name of analysis.
+#'
+#' @return A tibble with rows... columns...
+#'
+#' @export
+calculate_error_types <- function(dataset, alpha, analysis_name) {
+  # standard significance level
+  errors_tbl <- tibble::tibble(
+    Analysis = paste(analysis_name,
+                     "with significance",
+                     alpha),
+    true_positives = sum(dataset$significant & dataset$causal),
+    false_positives = sum(dataset$significant & !dataset$causal),
+    false_negatives = sum(!dataset$significant & dataset$causal),
+    true_negatives = sum(!dataset$significant & !dataset$causal)) %>%
+    tibble::add_row(
+      Analysis = paste(analysis_name,
+                       "with Bonferroni-correction"),
+      true_positives = sum(dataset$bonferroni & dataset$causal),
+      false_positives = sum(dataset$bonferroni & !dataset$causal),
+      false_negatives = sum(!dataset$bonferroni & dataset$causal),
+      true_negatives = sum(!dataset$bonferroni & !dataset$causal))
+
+  return(errors_tbl)
+}
