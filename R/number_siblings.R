d02d7dd029e96de5c61f5af565a29491e1ee6200
BigMag69 <79468396+BigMag69@users.noreply.github.com>
2021-05-25 12:35:54 +0200

    added n_sibs and build package

--- R/number_siblings.R
+++ R/number_siblings.R
@@ -1,4 +1,9 @@
-
-how_many_siblings_are_in_this_easily_openable_txt_file <- function(pheno_file){
+#' @title Title
+#'
+#' @param pheno_file phenofile.txt with path
+#'
+#' @return integer
+#' @export
+n_sibs <- function(pheno_file) {
   return(length(grep("sib", colnames(data.table::fread(pheno_file)))) / 3)
 }
