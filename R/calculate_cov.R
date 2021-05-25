ff1486e9fb3bbc801539979fe729b99ef6b186e6
jbirkesteen <jbsteenstrup@post.au.dk>
2021-05-25 15:57:56 +0200

    Changes to calculate_cov
    LInting, documented, closes #20, added return statement, changed variable names, built package.

--- R/calculate_cov.R
+++ R/calculate_cov.R
@@ -1,41 +1,37 @@
-#'
 #' @title Calculate covariances between liabilities
 #'
 #' @description Compute a sample covariance matrix. This can be used to
 #' validate that covariances of data are as expected.
 #'
 #'
-#' @param file path to the file that should be read.
-#' @param sibs number of siblings. optional. ÆNDRERS
+#' @param pheno dataset with phenotypes, as loaded by \code{load_phenotypes()}.
+#' @param sibs \emph{optional:} number of siblings to be included in covariance
+#' matrix. Defaults to using all of an individual's siblings.
+#'
+#' @return A covariance matrix using the data from the given dataset.
 #'
 #' @import stats
 #'
-#' @return A covariance matrix using the data from the selected file. All
-#' entries in the matrix are multiplied by 100.
-#'
 #' @export
-
-
-calculate_cov <- function(file, sibs) {
+calculate_cov <- function(pheno, sibs) {
   stopifnot("sib needs to be a non-negative integer" =
-            (!exists("sibs") || (sibs >= 0 && class(sibs) == "numeric"
+            (missing(sibs) || (sibs >= 0 && class(sibs) == "numeric"
                                  && round(sibs) == sibs)))
-  
-  ph <- data.table::fread(file)
-  ph <- as.data.frame(ph)
-  
-  if (missing(sibs)){  # OVERVEJELSE if sibs isn't specified we just assign the max number of sibs.
-    sibs <- n_sibs(ph)
+
+  # If sibs isn't specified we just assign the max number of siblings in data
+  if (missing(sibs)) {
+    sibs <- n_sibs(pheno)
   }
-  
-  ph[ph == -9] <- NA
+
+  pheno[pheno == -9] <- NA
   indexes <- c(c(4:5, 8), c(seq(11, 11 + sibs * 3, by = 3)))
-  
-  #This informs the user about the distribution of the siblings
+
+  # This informs the user about the distribution of the siblings
   message(noquote("Attention"))
-  for (i in 0:sibs){
-    message(noquote(paste0("number of families with at least ",i, " siblings: ",
-                     sum(complete.cases(ph[,c(seq(11, 11 + i * 3, by = 3))])))))
+  for (i in 0:sibs) {
+    message(noquote(
+      paste0("number of families with at least ", i, " siblings: ",
+             sum(complete.cases(pheno[, c(seq(11, 11 + i * 3, by = 3))])))))
   }
-  round(cov(ph[, indexes], use = "pairwise.complete.obs"), 2)
+  return(round(cov(pheno[, indexes], use = "pairwise.complete.obs"), 2))
 }
