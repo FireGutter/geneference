41bc2688a061780c4731dabec4564a0be4ec42a4
BigMag69 <79468396+BigMag69@users.noreply.github.com>
2021-05-25 14:18:31 +0200

    Update calculate_cov.R
    added warning message

--- R/calculate_cov.R
+++ R/calculate_cov.R
@@ -18,16 +18,24 @@
 
 calculate_cov <- function(file, sibs) {
   stopifnot("sib needs to be a non-negative integer" =
-            (!exists("sibs") || (sibs >= 0 && class(sibs) == "numeric" && round(sibs) == sibs)))
+            (!exists("sibs") || (sibs >= 0 && class(sibs) == "numeric"
+                                 && round(sibs) == sibs)))
   
   ph <- data.table::fread(file)
   ph <- as.data.frame(ph)
   
-  if (missing(sibs)){  # if sibs isn't specified we just assign the max number of sibs.
+  if (missing(sibs)){  # OVERVEJELSE if sibs isn't specified we just assign the max number of sibs.
     sibs <- n_sibs(ph)
   }
   
   ph[ph == -9] <- NA
   indexes <- c(c(4:5, 8), c(seq(11, 11 + sibs * 3, by = 3)))
-  round(100 * cov(ph[, indexes], use = "complete.obs"))
+  
+  #This informs the user about the distribution of the siblings
+  message(noquote("Attention"))
+  for (i in 0:sibs){
+    message(noquote(paste0("number of families with at least ",i, " siblings: ",
+                     sum(complete.cases(ph[,c(seq(11, 11 + i * 3, by = 3))])))))
+  }
+  round(cov(ph[, indexes], use = "pairwise.complete.obs"), 2)
 }
