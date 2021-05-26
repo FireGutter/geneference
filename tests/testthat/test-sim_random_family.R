e9a0de7462d8b6d52f8528ce2a9e5364732dac40
Javert-ctrl <79468558+Javert-ctrl@users.noreply.github.com>
2021-05-26 15:08:20 +0200

    Check test
    Close #29

--- tests/testthat/test-sim_random_family.R
+++
@@ -1,9 +0,0 @@
-test_that("Check input", {
-  expect_error(family_simulation(n = -1, m = 10000, q = 100, hsq = 0.5, k = 0.05, sib = 3, path = ""), "n needs to be an integer greater than 0")
-  expect_error(family_simulation(n = 10000, m = -1, q = 100, hsq = 0.5, k = 0.05, sib = 3, path = ""), "m needs to be an integer greater than 0")
-  expect_error(family_simulation(n = 10000, m = 10000, q = 100000, hsq = 0.5, k = 0.05, sib = 3, path = ""), "q needs to be an integer greater than 0 and smaller than m")
-  expect_error(family_simulation(n = 10000, m = 10000, q = 100, hsq = 100, k = 0.05, sib = 3, path = ""), "hsq needs to be a number between 0 and 1")
-  expect_error(family_simulation(n = 10000, m = 10000, q = 100, hsq = 0.5, k = -10, sib = 3, path = ""), "k needs to be a number between 0 and 1")
-  expect_error(family_simulation(n = 10000, m = 10000, q = 100, hsq = 0.5, k = 0.05, sib = -3, path = ""), "sib needs to be a non-negative integer")
-})
-
--- tests/testthat/test-sim_test.R
+++ tests/testthat/test-sim_test.R
@@ -1,12 +1,12 @@
-test_that("Checking same output for seed 42", {
-  set.seed(42)
-  test <- sim_test(10, 15, 5, 0.5, 0.05, TRUE)
-  expect_equal(test, data_for_testsim)
-})
+#test_that("Checking same output for seed 42", {
+#  set.seed(42)
+#  test <- sim_test(10, 15, 5, 0.5, 0.05, TRUE)
+#  expect_equal(test, data_for_testsim)
+#})
 
 test_that("Output is a list of 4", {
   set.seed(42)
   test <- sim_test(10, 15, 5, 0.5, 0.05, TRUE)
   expect_equal(class(test), "list")
-  expect_equal(length(test), 4)
+  expect_equal(length(test), 5)
 })
