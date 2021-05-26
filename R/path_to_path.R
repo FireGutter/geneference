74b86b9e6a49c76a49b03af8ede76f23f33d71fb
BigMag69 <79468396+BigMag69@users.noreply.github.com>
2021-05-26 13:31:38 +0200

    added description

--- R/path_to_path.R
+++ R/path_to_path.R
@@ -1,5 +1,8 @@
 #' @title Quote path
 #'
+#' @description takes a path string and puts it in quotes such that shell 
+#' doesn't get confused.
+#'
 #' @param path a path string \code{path//to//file}
 #'
 #' @return a path string in quotes \code{\"path//to//file\"}
