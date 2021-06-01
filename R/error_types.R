c11d54e67514a01197be272deaebf45c0c58d6b4
Rasmus <razz_bj@hotmail.com>
2021-06-01 04:04:01 +0200

    Update error_types.R
    Changed the description for standard significance and added change to documentation

--- R/error_types.R
+++ R/error_types.R
@@ -9,7 +9,9 @@
 #' and use \code{load_results()} to merge this with \code{beta.txt} from the
 #' simulation. Then, \code{augment_results()} needs to be run in order to obtain
 #' columns for information on which SNPs are causal, significant, and significant
-#' with Bonferroni correction.
+#' with Bonferroni correction. \cr
+#' The standard significance level refers to the one used in 
+#' \code{augment_results()}.
 #'
 #' @param causal column in dataset denoting causal SNPs.
 #' @param significant column in dataset denoting which SNPs are significant.
@@ -28,7 +30,7 @@ calculate_error_types <- function(causal, significant, bonferroni, analysis_name
   # standard significance level
   errors_tbl <- tibble::tibble(
     Analysis = paste(analysis_name,
-                     "with significance level used in augmentation of results"),
+                     "with standard significance level"),
     true_positives = sum(significant & causal),
     false_positives = sum(significant & !causal),
     false_negatives = sum(!significant & causal),
